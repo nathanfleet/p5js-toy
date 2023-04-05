c702a3fb06457c539b2e87467ec2d3692b274b9c
Nathan Fleet <nfleet@kent.edu>
2023-04-05 13:27:30 -0400

    made it cooler

--- line.js
+++ line.js
@@ -2,14 +2,18 @@ let prevX = null;
 let prevY = null;
 let currX = null;
 let currY = null;
+let buffer;
 
 function setup() {
   createCanvas(400, 400);
+  buffer = createGraphics(width, height);
 }
 
 function draw() {
   background(220);
+  image(buffer, 0, 0);
   if (prevX != null && prevY != null) {
+    stroke(random(255), random(255), random(255))
     line(prevX, prevY, currX, currY);
   }
 }
@@ -24,4 +28,6 @@ function mousePressed() {
   }
   currX = mouseX;
   currY = mouseY;
+  buffer.stroke(random(255), random(255), random(255));
+  buffer.line(prevX, prevY, currX, currY);
 }
